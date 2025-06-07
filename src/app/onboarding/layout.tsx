5e555a7906bd137163295f1d7c72f7f5cc8db53e
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-06-07 22:52:22 +0600

    Improvement: Code cleanup

--- src/app/onboarding/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DashboardLayout from '@/layouts/dashboard-layout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-      <DashboardLayout>{children}</DashboardLayout>
-  );
-}
--- src/app/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import OnboardingView from "@/sections/onboarding/views/onboarding-view";
-
-export const metadata = {
-  title: 'Onboarding',
-};
-
-export default function Page() {
-  return <OnboardingView />;
-}
