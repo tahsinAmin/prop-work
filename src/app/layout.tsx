d3a782d36d91b2a732715b633d93621fb990d131
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-06-07 21:25:31 +0600

    Improvement (Redux Toolkit & TTK): Provider added & font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,20 +5,32 @@ import CssBaseline from '@mui/material/CssBaseline';
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
 import ThemeProvider from "@/theme/index";
 import { primaryFont, secondaryFont } from '@/theme/typography';
+import { Provider } from "react-redux";
+import { store } from "../app/store";
 
-export default function RootLayout(props: { children: React.ReactNode }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-        <body>
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning className={`${primaryFont.variable} ${secondaryFont.variable}`}>
+      <head>
+        <style jsx global>{`
+          :root {
+            --font-poppins: ${primaryFont.style.fontFamily};
+            --font-barlow: ${secondaryFont.style.fontFamily};
+          }
+        `}</style>
+      </head>
+      <body>
+      <Provider store={store}>
         <InitColorSchemeScript attribute="class" />
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-            <ThemeProvider>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline/>
+          <ThemeProvider>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline/>
               {children}
-            </ThemeProvider>
+          </ThemeProvider>
         </AppRouterCacheProvider>
-        </body>
-        </html>
-    );
+      </Provider>
+      </body>
+    </html>
+  );
 }
